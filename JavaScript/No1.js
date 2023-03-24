b36cd024c78e1e1c568b19ac4afe5da9a3c5561f
NgurahErvan <114007640+NgurahErvan@users.noreply.github.com>
2023-03-24 23:08:59 +0800

    Create No1.js

---
+++ JavaScript/No1.js
@@ -0,0 +1,7 @@
+const Identitas = function() {
+    console.log("Saya Calon Admin MCI");
+    let time = 3000;
+    setTimeout(function() {
+      console.log("I Gusti Ngurah Ervan Juli Ardana"); }, time);
+  };
+  Identitas();
